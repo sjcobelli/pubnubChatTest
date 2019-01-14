90b8706227610e8854c55df683e95fd49ccb9fe3
Stefano Cobelli <stefanocobelli@FLBOCP0UKJGH6.local>
2019-01-14 15:00:01 -0500

    resolved chatengine error

--- src/app/engine/engine.component.ts
+++ src/app/engine/engine.component.ts
@@ -1,22 +1,22 @@
 import { Component, OnInit } from "@angular/core";
 import { Observable } from "rxjs";
-import {ChatEngineCore} from "chat-engine"
+
+var pnConfig = require("../../pubnub.config.json");
+var ChatEngineCore = require("chat-engine");
+
 
 @Component({
   selector: "app-engine",
   templateUrl: "./engine.component.html",
   styleUrls: ["./engine.component.css"]
 })
+
 export class EngineComponent implements OnInit {
   myChat;
   publish;
   ChatEngine;
   constructor() {
-    let ChatEngine;
-    ChatEngine= ChatEngineCore.create({
-      publishKey: "pub-c-30964bce-3955-4383-a5bd-a400af9d9f64",
-      subscribeKey: "sub-c-a4ab9daa-1454-11e9-9cda-0ee81137d4bc"
-    });
+    this.ChatEngine= ChatEngineCore.create(pnConfig);
   }
 
   ngOnInit() {}
@@ -53,54 +53,7 @@ export class EngineComponent implements OnInit {
   messages: string[] = [];
 
   connect() {
-      me = this.ChatEngine.connect('sbasavanahally@gmail.com', {color: "green"});
-      this.ChatEngine.on('$.ready', (data) => {
-        //TODO: Add reconnection features
-        let me = data.me
-    
-        me.direct.on('$.invite', (payload) => {
-    
-            let chat = new ChatEngine.Chat(payload.data.channel);
-    
-            chat.on('$.connected', (payload) => {
-                appendMessage(me.uuid, 'Connected to chat!');
-            });
-    
-            chat.on('$.online.here', (payload) => {
-                appendMessage(
-                'Status',
-                payload
-                .user.uuid + ' is in the channel! Their color is ' + payload.user.state.color + '.'
-            )
-                ;
-            })
-                ;
-    
-            chat.on('$.online.join', (payload) => {
-                appendMessage('Status', payload.user.uuid + ' has come online! Their color is ' + payload.user.state.color + '.'
-            )
-                ;
-            })
-                ;
-    
-            chat.on('message', (payload) => {
-                appendMessage(payload.sender.uuid, payload.data.text
-            )
-                ;
-            })
-                ;
-    
-            $("#message").keypress(function (event) {
-                if (event.which == 13) {
-                    chat.emit('message', {
-                        text: $('#message').val()
-                    });
-                    $("#message").val('');
-                    event.preventDefault();
-                }
-            });
-    });
-    });
+      
   }
 
 
